2206b7311a2cbd3621de96069942338e71a8e380
Jonatan <jonatanblue@users.noreply.github.com>
2022-06-19 18:16:17 +0100

    Add broken client code to main

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
 	twitter "github.com/g8rswimmer/go-twitter/v2"
+
+	client "github.com/jonatanblue/tweet-operator/pkg/client/clientset/versioned"
 )
 
 type authorize struct {
@@ -33,6 +39,25 @@ func mustLookupEnv(key string) string {
 	In order to run, the user will need to provide the bearer token and the list of ids.
 **/
 func main() {
+
+	var kubeconfig *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Printf("Building config from flags, %s", err.Error())
+	}
+
+	client := client.NewForConfigOrDie(config)
+
+	log.Printf("Client: %v", client)
+
 	token := mustLookupEnv("TWITTER_BEARER_TOKEN")
 	ids := flag.String("ids", "", "twitter ids")
 	flag.Parse()
